2c991b2b35bf16b6eeadf803a86daf43cc7a586f
trg5pdx <trg5@pdx.edu>
2023-02-28 16:25:20 -0800

    Set up the skeleton for the networking type class

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,25 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Concurrent (forkFinally)
+import qualified Control.Exception as E
+import Control.Monad (unless, forever, void)
+import qualified Data.ByteString as S
+import Network.Socket as N
+import Network.Socket.ByteString (recv, sendAll)
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+class Networking s where
+  -- connect :: 
+  -- bind ::
+  listen :: Socket -> s ()
+  -- accept ::
+  -- send ::
+  -- recv :: 
+  -- close ::
+
+  
+instance Networking IO where
+  listen soc = N.listen soc 1024
